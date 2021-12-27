66c1f346c15f4421319a36178b40815c3bda332c
DexterWymiatacz <dexterwymiatacz@gmail.com>
2021-12-27 03:17:42 +0100

    portfolio to resume switch

--- scripts/float_buttons.js
+++ scripts/float_buttons.js
@@ -1,21 +1,27 @@
-const buttonSection = document.querySelector(".float-button")
+let btn1 = document.querySelector(".btn1");
+let btn2 = document.querySelector(".btn2");
+let portfolio = document.querySelector(".porfolio-wrapper");
+let resume = document.querySelector(".resume");
 
-const buttonSectionOptions = {
-  rootMargin: "-961px 0px 0px 0px"
-};
+btn1.addEventListener("click", changeViewPorfolio);
+btn2.addEventListener("click", changeViewResume);
 
-const buttonSectionObserver = new IntersectionObserver(function(
-  entries,
-  buttonSectionObserver
-) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      buttonSection.classList.add("floating");
-    } else {
-      buttonSection.classList.remove("floating");
-    }
-  });
-},
-buttonSectionOptions);
+function changeViewPorfolio() {
+  if(resume.style.display == "none") {}
+  else {
+    btn1.classList.add("active-view");
+    resume.style.display = "none";
+    btn2.classList.remove("active-view");
+    portfolio.style.display = "block";
+  }
+}
 
-buttonSectionObserver.observe(buttonSection);
+function changeViewResume() {
+  if(portfolio.style.display == "none") {}
+  else {
+    btn2.classList.add("active-view");
+    portfolio.style.display = "none";
+    btn1.classList.remove("active-view");
+    resume.style.display = "block";
+  }
+}
