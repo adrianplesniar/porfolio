cc60020b5690ce5a730749bba02b03089efa10c7
DexterWymiatacz <dexterwymiatacz@gmail.com>
2022-01-13 05:27:54 +0100

    parallax mobile

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -4,6 +4,11 @@ let scrollBtn = document.querySelector('.scroll-btn');
 window.addEventListener('scroll', function() {
     let value = window.scrollY;
 
-    headerText.style.top = value * 0.5 + 'px';
     scrollBtn.style.top = value * 0.1 + 'px';
+
+    if (window.matchMedia("(max-width: 600px)").matches) {
+        headerText.style.top = value * 0.3 + 'px';
+      } else {
+        headerText.style.top = value * 0.5 + 'px';
+      }
 })
